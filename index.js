869b20373f160fa2a9b05d44e22536948d71419e
Vishnu Paul <paulvishnu567@gmail.com>
2021-05-13 21:21:10 +0530

    make a form to take contact input

--- index.js
+++ index.js
@@ -6,9 +6,43 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded());
+
+var contactList = [
+  {
+    name: 'Paul',
+    phone: '1234567891',
+  },
+  {
+    name: 'iron man',
+    phone: '56423176231',
+  },
+  {
+    name: 'Hulk',
+    phone: '9999999990',
+  },
+];
 
 app.get('/', (req, res) => {
-  return res.render('home');
+  return res.render('home', {
+    title: 'My contact list',
+    contact_list: contactList,
+  });
+});
+app.get('/practice', (req, res) => {
+  return res.render('practice', {
+    title: 'Let us play',
+  });
+});
+
+app.post('/create-contact', (req, res) => {
+  // contactList.push({
+  //   name: req.body.name,
+  //   phone: req.body.phone,
+  // });
+  contactList.push(req.body);
+  // return res.redirect('/');
+  return res.redirect('back');
 });
 
 app.listen(port, (err) => {
